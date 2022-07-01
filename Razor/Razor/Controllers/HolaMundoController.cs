6df7837eb6325241784fb19dc90bd7eae2f75aef
Jesus231299 <jesus.of.999@hotmail.com>
2022-06-30 19:04:39 -0500

    CommitInicial
    Subida de archivos

---
+++ Razor/Razor/Controllers/HolaMundoController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Razor.Controllers
+{
+    public class HolaMundoController : Controller
+    {
+        // GET: HolaMundo
+        public ActionResult Index()
+        {
+            ViewBag.bienv = "Bienvenido";
+
+            ViewBag.day = "Hoy es " + DateTime.Now;
+
+            return View();
+        }
+    }
+}
